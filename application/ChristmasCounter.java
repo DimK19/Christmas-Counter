9b5861a544eb44ae805801968b00e29fe70127c5
DimK19 <kyriakidis_dimitrios@outlook.com>
2021-06-16 01:52:05 +0300

    Change string arrays to be populated procedurally

--- application/ChristmasCounter.java
+++ application/ChristmasCounter.java
@@ -18,24 +18,21 @@ public class ChristmasCounter
 	Color CGREEN = new Color(0, 64, 0);
 	String[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September",
 			"October", "November", "December" };
-	String[] days = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18",
-			"19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" };
-	String[] years = { "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991",
-			"1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004",
-			"2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017",
-			"2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030",
-			"2031", "2032", "2033", "2034", "2035", "2036", "2037", "2038", "2039", "2040", "2041", "2042", "2043",
-			"2044", "2045", "2046", "2047", "2048", "2049", "2050", "2051", "2052", "2053", "2054", "2055", "2056",
-			"2057", "2058", "2059", "2060", "2061", "2062", "2063", "2064", "2065", "2066", "2067", "2068", "2069",
-			"2070", "2071", "2072", "2073", "2074", "2075", "2076", "2077", "2078", "2079", "2080" };
+	String[] days, years;
 	JLabel instructionLabel, answerLabel, imageLabel;
 	JComboBox<String> monthBox, dayBox, yearBox;
 	ImageIcon image;
 	JFrame frame;
 	JButton finalOKButton;
-
+	
 	ChristmasCounter()
 	{
+	    // initialize string arrays
+	    days = new String[31];
+	    years = new String[101];
+	    for(int i = 0; i < 31; ++i) days[i] = Integer.toString(i + 1);
+	    for(int i = 0; i <= 100; ++i) years[i] = Integer.toString(i + 1980);
+	    
 		this.frame = new JFrame("Christmas Counter");
 
 		this.frame.setLayout(new FlowLayout());
